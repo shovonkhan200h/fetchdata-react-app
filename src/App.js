a93dde02f7e7e405290b790d96896aecdac27aa1
shovonkhan200h <shovonkhan00@gmail.com>
2023-10-11 21:41:08 +0100

    fetch-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect, useState} from 'react'
+const nayok=[{name:`Md Mazharul Islam`, job:`front-end-developer`},
+{name:`nabila`, job:`tilugiri`}]
+const names = [`shovon`,`nabila`]
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {
+        nayok.map(item => <MyName name={item.name} job={item.job}></MyName>)
+      }
+
+      <MyData></MyData>
       </header>
     </div>
   );
 }
-
+const MyData =()=>{
+  const [user,setUser]= useState([])
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res=>res.json())
+    .then(data=> setUser(data))
+  },[])
+  return(
+    <div>
+      {
+        user.map(item=> {
+          return <div>
+            <li>{item.name}</li>
+            <li>{item.email}</li>
+          </div>
+          
+        })
+      }
+    </div>
+  )
+}
+const MyName=(props)=>{
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>{props.job}</p>
+      </div>
+    )
+}
 export default App;
